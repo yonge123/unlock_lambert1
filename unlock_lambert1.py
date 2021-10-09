c09a50e732b48f31dd989d96f3e55deb9b1904ff
jyhome <yonge123@gmail.com>
2021-10-09 12:13:44 +0900

    update

--- unlock_lambert1.py
+++ unlock_lambert1.py
@@ -1,5 +1,16 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import maya.cmds as cmds
 import maya.OpenMaya as om
+import locale
+
+_LOCALE, _ = locale.getdefaultlocale()
+
+if _LOCALE == 'ko_KR':
+    _UNLOCK_MSG = u" lambert1 이 lock 되어있는 문제를 수정하였습니다.\n 수정한 scene 을 저장하십시오."
+else:
+    _UNLOCK_MSG = u' The lambert1 locking issue has been solving.\n Save the scene file, please.'
 
 _INITIAL_SHADERS = ['initialShadingGroup', 'initialParticleSE', 'lambert1']
 
@@ -14,13 +25,15 @@ def iterLockedInitialShaders():
 
 
 def unLockInitialShaders():
-    for node in iterLockedInitialShaders():
-        print('unlock node -> "{}"'.format(node))
-        cmds.lockNode(node, l=False, lu=False)
+    lockShaders = list(iterLockedInitialShaders())
+    if lockShaders:
+        for node in lockShaders:
+            print('unlock node -> "{}"'.format(node))
+            cmds.lockNode(node, l=False, lu=False)
+
+        cmds.inViewMessage(bkc=0x09e2e2e, msg=_UNLOCK_MSG, fst=3200, fade=6, fts=14, font='Arial', pos='midCenterTop')
 
 
 def beforeSaveCallback():
     # Unlock Lambert1 before SceneSaved
     om.MSceneMessage.addCallback(om.MSceneMessage.kBeforeSave, unLockInitialShaders)
-
-
--- userSetup.py
+++ userSetup.py
@@ -1,8 +1,8 @@
 import maya.cmds as cmds
 
 # Unlock Lambert1 after SceneOpened
-cmds.scriptJob(event=["SceneOpened", "import time;time.sleep(0.3);unLockInitialShaders()"], protected=True)
+cmds.scriptJob(event=["SceneOpened", "import time;time.sleep(0.3);import unlock_lambert1;unlock_lambert1.unLockInitialShaders()"], protected=True)
 
 # Unlock Lambert1 Before SceneSaved
-cmds.scriptJob(event=["idle", "beforeSaveCallback()"], runOnce=True)
+cmds.scriptJob(event=["idle", "import unlock_lambert1;unlock_lambert1.beforeSaveCallback()"], runOnce=True)
 
