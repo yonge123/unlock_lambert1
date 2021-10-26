e292bd14a23e6906247c8b1b2bc45d4a4ea7e8a2
JeongYongHwang <yonge123@gmail.com>
2021-10-26 15:13:25 +0900

    update

--- unlock_lambert1.py
+++ unlock_lambert1.py
@@ -24,7 +24,7 @@ def iterLockedInitialShaders():
                 yield node
 
 
-def unLockInitialShaders():
+def unLockInitialShaders(*args):
     lockShaders = list(iterLockedInitialShaders())
     if lockShaders:
         for node in lockShaders:
